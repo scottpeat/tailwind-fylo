7bd58f0d9b72ec7bf743b970fc39cadb46abb2cf
Scott Peat <scottpeatweb@gmail.com>
2025-07-22 13:51:37 +0100

    Writing JavaScript for the color mode picker for light/dark mode

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+      if (
+        localStorage.getItem('color-theme') === 'dark' ||
+        (!('color-theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ) {
+        // Show Light Icon 
+        themeToggleLightIcon.classList.remove('hidden')
+      } else {
+        themeToggleDarkIcon.classList.remove('hidden')
+      }
