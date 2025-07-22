544849e10165d0af7cbccc72cc8f6a7b9efc05b9
Scott Peat <scottpeatweb@gmail.com>
2025-07-22 09:57:18 +0100

    Updated tailwind.config for dark mode and added fonts into HTML code.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -19,7 +20,15 @@ module.exports = {
       backgroundImage: (theme) => ({
         'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
         'logo-light-mode': "url('../images/logo-light-mode.svg')",
-      })
+        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
+      }),
+    },
+
+    variants: {
+      extend: {
+        backgroundImage: ['dark'],
+      },
     },
     plugins: [],
   },
